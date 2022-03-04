16347ff318dcc4b20e80f0e8da1e1b76edb4ab82
annabelng <a.h.ng952@gmail.com>
2022-03-03 23:36:58 -0800

    fix splits

--- training.py
+++ training.py
@@ -2,7 +2,24 @@ from datasets import load_dataset
 
 basedir = '/Users/annabelng/Personal Items/Personal/MAP/notebooks'
 
-input_dataset = load_dataset('csv', data_files = basedir + '/data/processed_text.csv')
-label_dataset = load_dataset('csv', data_files = basedir + '/data/labels.csv')
+input_dataset = load_dataset('csv', data_files = {
+    'train': basedir + '/data/train_text.csv',
+    'test': basedir + '/data/test_text.csv'
+    })
+
+label_dataset = load_dataset('csv', data_files = {
+    'train': basedir + '/data/train_label.csv',
+    'test': basedir + '/data/test_label.csv'
+    })
 
 print(label_dataset)
+'''
+train_testvalid = text_dataset['train'].train_test_split(test = 0.2)
+test_valid = train_testvalid['test'].train_test_split(test = 0.5)
+train_testvalid_text = DatasetDict({
+    'train_text': train_testvalid['train'],
+    'test_text': test_valid['test'],
+    'valid_text': test_valid['train']
+    })
+print(train_testvalid_text)
+'''
